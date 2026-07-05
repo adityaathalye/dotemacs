1954c291f0a8c666a2dd307cfd10bc266ec49195
Aditya Athalye <aditya.athalye@gmail.com>
2026-07-05 14:07:39 +0530

    llms: Set latest local Qwen3.x coding & reasoning. Default to
    local Qwen coding model for local-only LLM interactions.

--- init-adi-llms.el
+++ init-adi-llms.el
@@ -10,19 +10,27 @@
 (use-package gptel
   ;; ref: https://github.com/karthink/gptel?tab=readme-ov-file
   :config
+  (setq
+   gptel-model 'qwen3.6:latest
+   gptel-backend (gptel-make-ollama "Ollama"
+                   :host "localhost:11434"
+                   :stream t
+                   :models '(;; NAME                  ID              SIZE    ; ollama list
+                             qwen3.6:27b           ;; a50eda8ed977    17 GB
+                             qwen3.6:latest        ;; 07d35212591f    23 GB
+                             qwen3-coder:30b       ;; 06c1097efce0    18 GB
+                             smollm2:135m          ;; 9077fe9d2ae1    270 MB
+                             qwen2.5:1.5b          ;; 65ec06548149    986 MB
+                             qwen2.5-coder:1.5b    ;; d7372fd82851    986 MB
+                             )))
+
+  ;; OPTIONAL configuration
   (load-file adi/llms-api-configs-file)
   (gptel-make-openai "Groq" ;; Groq offers an OpenAI compatible API
     :host "api.groq.com"
     :endpoint "/openai/v1/chat/completions"
     :stream t
-    :key (lambda () (adi/get-env-secret "GROQ_API_KEY")))
-  (gptel-make-ollama "Ollama"
-    :host "localhost:11434"
-    :stream t
-    :models '(smollm2:135m
-              qwen2.5:1.5b
-              qwen2.5-coder:1.5b)))
+    :key (lambda () (adi/get-env-secret "GROQ_API_KEY"))))
 
 (provide 'adi-init-llms)
 ;;; adi-init-llms.el ends here
-
